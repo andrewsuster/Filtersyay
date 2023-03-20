42bf88570eaed8f011a3af1e7c96fabcc321a629
andrewsuster <126193001+andrewsuster@users.noreply.github.com>
2023-03-19 22:01:45 -0700

    initial commit broh

---
+++ swiftuicameraexample/swiftuicameraexample/swiftuicameraexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuicameraexampleApp.swift
+//  swiftuicameraexample
+//
+//  Created by Andrew Suster on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuicameraexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
